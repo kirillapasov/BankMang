3420b1c083a46268e7582c4e3ec482cac0a3d76e
Kirill Apasov <kirill.apasov@yandex.ru>
2023-12-04 23:20:09 +0300

    Я не помню, что я пофиксил, я потом много тестил, но всё работает, теперь остаётся БД и Фронт

--- src/main/java/com/example/BankMang/controller/UserController.java
+++ src/main/java/com/example/BankMang/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -17,7 +19,10 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         User user = userService.getUserById(id);
