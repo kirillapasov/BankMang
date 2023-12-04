4a6a31b922c74cd1f3080fd30862a1d56946d26d
Kirill Apasov <kirill.apasov@yandex.ru>
2023-12-04 15:33:07 +0300

    Добавлен контроллер для работы с сущностью кредитное предложение

---
+++ src/main/java/com/example/BankMang/controller/CreditOfferController.java
@@ -0,0 +1,52 @@
+package com.example.BankMang.controller;
+import model.CreditOffer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.BankMang.service.CreditOfferService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/credit-offers")
+public class CreditOfferController {
+
+    private final CreditOfferService creditOfferService;
+
+    @Autowired
+    public CreditOfferController(CreditOfferService creditOfferService) {
+        this.creditOfferService = creditOfferService;
+    }
+
+    @GetMapping
+    public List<CreditOffer> getAllCreditOffers() {
+        return creditOfferService.getAllCreditOffers();
+    }
+
+    @GetMapping("/{creditOfferId}")
+    public ResponseEntity<CreditOffer> getCreditOfferById(@PathVariable long creditOfferId) {
+        return creditOfferService.getCreditOfferById(creditOfferId)
+                .map(creditOffer -> new ResponseEntity<>(creditOffer, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<CreditOffer> createCreditOffer(@RequestBody CreditOffer creditOffer) {
+        CreditOffer createdCreditOffer = creditOfferService.createCreditOffer(creditOffer);
+        return new ResponseEntity<>(createdCreditOffer, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{creditOfferId}")
+    public ResponseEntity<CreditOffer> updateCreditOffer(@PathVariable long creditOfferId, @RequestBody CreditOffer updatedCreditOffer) {
+        return creditOfferService.updateCreditOffer(creditOfferId, updatedCreditOffer)
+                .map(creditOffer -> new ResponseEntity<>(creditOffer, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{creditOfferId}")
+    public ResponseEntity<Void> deleteCreditOffer(@PathVariable long creditOfferId) {
+        boolean deleted = creditOfferService.deleteCreditOffer(creditOfferId);
+        return deleted ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
